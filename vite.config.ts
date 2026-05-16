ba38633dba816efe582f3eb2bbed7dd7af69693c
Mihály Orsós <ormraat.pte@gmail.com>
2026-05-16 18:28:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/movemax/",
+  base: "/",
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
